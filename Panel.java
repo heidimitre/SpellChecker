415a14ef941b5f597e5bf4c230511b2490aefa03
heidimitre <heidi.mitre@gmail.com>
2017-03-06 14:07:22 -0700

    Finished button listeners for add and ignore words

--- Panel.java
+++ Panel.java
@@ -1,16 +1,19 @@
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 import javax.swing.event.*;
 import java.util.*;
 
 public class Panel extends JPanel
  {
-  	JButton up1;
+	public static TextCompare comparator = new TextCompare();
+	public static Vector<Word> wordVector;
+	JButton up1;
 	JTextField text1;
 	JTextField text2;
-	JList words;
-	TextCompare comparator = new TextCompare();
+	JTextField currentWord;
  
  	public Panel()
  	{
@@ -30,6 +33,8 @@ public class Panel extends JPanel
  		JPanel buttonP = new JPanel();
  		buttonP.setLayout(new FlowLayout());
  		buttonP.add(addB);
+ 		addB.addActionListener(new ButtonListener());
+ 		igB.addActionListener(new ButtonListener());
  		buttonP.add(igB);
  		left.add(top, BorderLayout.NORTH);
  		left.add(buttonP,BorderLayout.SOUTH);
@@ -40,7 +45,7 @@ public class Panel extends JPanel
  		up1 = new JButton("Upload Files");
 		text1 = new JTextField("sampleText.txt");
 		text2 = new JTextField("testDictionary.txt");
- 		JButton save = new JButton("Save All");
+
  		JPanel right = new JPanel();
  		right.setLayout(new BorderLayout());
  		JPanel bP = new JPanel();
@@ -49,8 +54,8 @@ public class Panel extends JPanel
 		bP.add(text1);
 		bP.add(text2);
  		bP.add(up1);
+ 		up1.addActionListener(new ButtonListener());
  		right.add(bP, BorderLayout.NORTH);
- 		right.add(save, BorderLayout.SOUTH);
 
  		//center of top panel (empty)
  		JPanel center = new JPanel();
@@ -64,32 +69,80 @@ public class Panel extends JPanel
 		JPanel south = new JPanel();
 		south.setLayout(new BorderLayout());
 		JLabel title = new JLabel("Add/Ignore Word");
-		JTextField list = new JTextField();
+		currentWord = new JTextField();
+		currentWord.setEditable(false);
 
 		JPanel ex = new JPanel();
 		ex.setLayout(new FlowLayout());
 		JButton export = new JButton("Export Dictionary");
 		ex.add(export);
 		south.add(title, BorderLayout.NORTH);
-		south.add(list, BorderLayout.CENTER);
+		south.add(currentWord, BorderLayout.CENTER);
 		south.add(ex, BorderLayout.SOUTH);
-		//*********vector is input of JList!!!************
-		words = new JList();
-		JScrollPane scr = new JScrollPane(words);
 
 		//add to outermost panel
  		add(north);
  		add(south);
  	}
+ 
+	 private class ButtonListener implements ActionListener
+	 {
+			public void actionPerformed(ActionEvent event){
+				String source = event.getActionCommand();
+				if(source.equals("Upload Files"))
+				{
+					String textFile = text1.getText();
+					String dictionaryFile = text2.getText();		
+					wordVector = comparator.compareText(textFile,dictionaryFile);
+					comparator.printWords();
+					updateUIElements();
+				}
+				if(source.equals("Add Word"))
+				{
+					 boolean doneSearching = false;
+				      int index = 0;
+				      while(index < wordVector.size() && doneSearching == false){
+				        if(wordVector.get(index).isFound == false){
+				          wordVector.get(index).isFound = true;
+				          doneSearching = true;
+				          comparator.addWordToDictionary(wordVector.get(index));
+				        }
+				        index++;
+				      }
+				      updateUIElements();
+				}
+				if(source.equals("Ignore Word"))
+				{
+					boolean doneSearching = false;
+				    int index = 0;
+				    while(index < wordVector.size() && doneSearching == false){
+				    	if(wordVector.get(index).isFound == false)
+				    	{
+				    		wordVector.get(index).isFound = true;
+				    		doneSearching = true;
+				    		comparator.ignoreWord(wordVector.get(index));
+				        }
+				        index++;
+				      }
+				    updateUIElements();
+				 }
 
-	public void actionPerformed(ActionEvent event){
-		String source = event.getActionCommand();
-		
-		text1.setText(source);
-		if(source.equals("Upload Text File")){
-			String textFile = text1.getText();
-			String dictionaryFile = text2.getText();		
-			comparator.compareText(textFile,dictionaryFile);
-		}
-	}
+			}
+	 }
+	 
+	 public void updateUIElements(){
+		    boolean doneSearching = false;
+		    int index = 0;
+		    while(index < wordVector.size() && doneSearching == false){
+		      if(wordVector.get(index).isFound == false){
+		        doneSearching = true;
+		        currentWord.setText(wordVector.get(index).text);
+		      }
+		      index++;
+		    }
+		    if(doneSearching == false){
+		      currentWord.setText("All words in file have been checked!");
+		      comparator.printDictionaryWords();
+		    }
+		  }
  }
